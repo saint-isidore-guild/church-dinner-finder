5c9e8ce21c86d72fa3ac0c7af40f2b2e6a2d03d7
Mike Truso <mike@agileorbit.com>
2021-12-30 00:09:11 -0600

    nuxt project with content module, load events from json file

---
+++ cdf-nuxt/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <div v-for="event in events" :key="event.slug">
+            <h3>{{event.title}}</h3>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData ({ $content }) {
+    const events = await $content('events/2022')
+      .sortBy('startDate', 'asc')
+      // .where({categories: {$in: ['fish']} })
+      .fetch()
+
+    return {
+      events
+    }
+  }
+}
+</script>
